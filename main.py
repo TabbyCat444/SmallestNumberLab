3b1bb9c2a60656245b1a4d3a78162db3c43e2785
Tabitha <missinlucky004@aol.com>
2022-10-20 16:08:28 -0500

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
+num_3 = int(input("Please enter one more number: "))
+
+num_list = [num_1, num_2, num_3]
+
+low_num = min(num_list)
+
+print("The smallest number is:", + low_num)
